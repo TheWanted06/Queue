5a668ff5acb690a6f67a7b9272d11b015d795a53
Daniel <tshipukdaniel@gmail.com>
2021-09-15 14:50:27 +0200

    second commit

---
+++ Queque/IncomingCall.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queque
+{
+    class IncomingCall
+    {
+        public int Id { get; set; }
+
+        public int ClientId { get; set; }
+
+        public DateTime CallTime { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string Consultant { get; set; }
+
+        //add a new prop-->
+        public bool isPriority { get; set; }
+    }
+}
+
---
+++ Queque/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace Queque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallCenter center = new CallCenter();
+            Parallel.Invoke(
+                () => CallersAction(center),
+                () => ConsultantAction(center, "Nashy", ConsoleColor.Red),
+                () => ConsultantAction(center, "Liv", ConsoleColor.Yellow),
+                () => ConsultantAction(center, "Jan", ConsoleColor.Green));
+        }
+
+        private static void CallersAction(CallCenter center)
+        {
+            Random random = new Random();
+            while (true)
+            {
+                int clientId = random.Next(1, 10000);
+                int waitingCount = http://center.call/(clientId);
+                Log($"Incoming call from {clientId}, waiting in the queue: {waitingCount}");
+                Thread.Sleep(random.Next(1000, 5000));
+            }
+        }
+
+        private static void Log(string v)
+        {
+            Console.WriteLine(v);
+        }
+
+        private static void ConsultantAction(CallCenter center, string name, ConsoleColor color)
+
+        {
+            Random random = new Random();
+            while (true)
+            {
+                IncomingCall call = center.Answer(name);
+                if (call != null)
+                {
+                    Console.ForegroundColor = color;
+                    Log($"Call #{http://call.id/} from {call.ClientId} is answered by {call.Consultant}.");
+                    Console.ForegroundColor
+                }
+            }
+        }
+    }
+}
